68227bad94ba4337135ee32cd1fcc068a9c7f18f
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-04-16 12:59:31 -0400

    Added DB adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
+import clientPromise from "@/lib/mongodbadapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 export default NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!, //Guarantee that it won't be null
